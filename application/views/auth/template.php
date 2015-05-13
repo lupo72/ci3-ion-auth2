813c9dd2d5fa702da272e2df1cbf7800de1dfe27
Lupo <w-pfeiffer-tue@gmx.de>
2015-05-14 00:28:56 +0200

    Initial Commit

---
+++ application/views/auth/template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>CodeIgniter 3.0 with Ion-Auth 2.0 </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            table {
+                border: 1px solid black;
+                font-family: verdana;
+                font-size-adjust: 12    px;
+                font-size: 12px;
+            }
+           
+            td {
+                padding: 0px 5px 0px 5px;
+                border: 1px solid lightgrey;
+            }
+
+            a { color: #0134c5 }
+            a:visited { color: #0134c5 }
+            
+            
+        </style>
+    </head>
+    <body>
+        <div>
+            <?php echo $content; ?>
+        </div>
+    </body>
+</html>
---
+++ application/views/no-access.php
@@ -0,0 +1,11 @@
+<h2>
+    You need an Developer Account to Access to this page.
+</h2>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
